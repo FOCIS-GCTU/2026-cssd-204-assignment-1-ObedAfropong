c5007daf06b1fb4b346b5b9cd985f1420ab54ad9
ObedAfropong <2425401973@live.gctu.edu.gh>
2026-05-27 21:53:33 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Obed Afropong
+# STUDENT ID:  2425401973
+# Email: 2425401973@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Obed Afropong , this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
+    #creating an empty line  using print but not variable
+    print()
+    #First print of the initials
+    print("...OAC")
+    print()
+    #printing my iniitals in block letters OAC
+    print("...OOOOOOOOOOOO........AAAAAAAAAAAA........CCCCCCCCCCCC.....")
+    print("...OO........OO........AA........AA........CC...............")
+    print("...OO........OO........AA........AA........CC...............")
+    print("...OO........OO........AA........AA........CC...............")
+    print("...OO........OO........AAAAAAAAAAAA........CC...............")
+    print("...OO........OO........AA........AA........CC...............")
+    print("...OO........OO........AA........AA........CC...............")
+    print("...OO........OO........AA........AA........CC...............")
+    print("...OO........OO...**...AA........AA...**...CC.............**")
+    print("...OOOOOOOOOOOO...**...AA........AA...**...CCCCCCCCCCCC...**")
+    print()
 
 main()
+
